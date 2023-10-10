b35c2cb2d1aa8d5e2b380a13fbd8b74ebd66d403
albertostizzoli <albertostizzoli@yahoo.it>
2023-10-10 14:45:39 +0200

    Begin Conditions

--- js/script.js
+++ js/script.js
@@ -4,3 +4,20 @@ const discountUnder18 = 20/100;
 const discountOver65 = 40/100;
 let price = kmPrice * km;
 let message = 'Il prezzo del biglietto è:';
+
+// Bottone Genera
+
+const button = document.querySelector('.btn-primary');
+console.log(button);
+
+button.addEventListener('click', function(){
+    let nome = document.getElementById('nome').value;
+    let km = document.getElementById('km').value;
+    let eta = document.getElementById('eta').value;
+
+    //Condizioni
+    
+
+
+}
+);
